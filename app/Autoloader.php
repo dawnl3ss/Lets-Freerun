bf71a9488493541cf4014a2e5fb4dcbb565249db
Neptune-IT <neptune.dev.cybsec@gmail.com>
2022-01-30 01:02:49 +0100

    Revert "Revert "google map api setup""
    This reverts commit 05870b8d7e3c2425eee02207d5394f6d7d03db17.

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -5,7 +5,7 @@ function __load_all_classes() : void {
         if ($folders != "." and $folders != ".." and $folders != "Autoloader.php"){
             foreach (@scandir("app/{$folders}") as $file){
                 if ($file != "." and $file != "..") {
-                    require_once "app/{$folders}/{$file}";
+                    if (is_file("app/{$folders}/{$file}")) require_once "app/{$folders}/{$file}";
                 }
             }
         }
