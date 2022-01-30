05870b8d7e3c2425eee02207d5394f6d7d03db17
Neptune-IT <neptune.dev.cybsec@gmail.com>
2022-01-30 01:02:27 +0100

    Revert "google map api setup"
    This reverts commit 9217e059fa5700f3c26f966ec7ed0e0d0bd813e3.

--- app/location/maps/google-maps-api.js
+++
@@ -1,9 +0,0 @@
-export function display_map(lat, long) {
-    var options = {
-        center: new google.maps.LatLng(lat, long),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(document.getElementById("map"), options);
-}
