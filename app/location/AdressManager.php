cfbc5b646903f1d5ff79e4ca5f29243fb03b3b1d
Atsuda <85411806+Ats12MC@users.noreply.github.com>
2022-08-01 11:56:21 +0200

    Update AdressManager.php

--- app/location/AdressManager.php
+++ app/location/AdressManager.php
@@ -1,17 +1,8 @@
 <?php
 
 /**
- * @param string $data
- *
  * @param string $ip
  *
  * @return mixed
  */
-function get_data_by_ip(string $data, string $ip){ return unserialize(file_get_contents("http://ip-api.com/php/" . $ip))[$data]; }
-
-/**
- * @param string $ip
- *
- * @return string
- */
-function get_current_country(string $ip) : string { return get_data_by_ip("country", $ip); }
+function get_current_country(string $ip){ return unserialize(file_get_contents("http://ip-api.com/php/" . $ip))["country"]; }
